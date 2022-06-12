cced06eebd6554c62af9aafaacd8196ac3d4443f
saulbarrios <barriossaul@gmail.com>
2022-06-11 20:53:55 -0500

    Cotizador de Seguros de Autos Done

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,12 @@
+import "../styles/Spinner.css";
+
+const Spinner = () => {
+  return (
+    <div className="spinner">
+      <div class="dot1"></div>
+      <div class="dot2"></div>
+    </div>
+  );
+};
+
+export default Spinner;
